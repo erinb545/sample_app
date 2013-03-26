cb42ea2fc2493e787fa9b5d24f2634e878c554fe
erinb545 <erin.fitzhenry@gmail.com>
2013-03-26 15:10:03 -0700

    Fixed the users tests.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,12 +2,26 @@ require 'spec_helper'
 
 describe "User pages" do
 
-  subject { page }
+  #subject { page }
+
+  #describe "signup page" do
+  #  before { visit signup_path }
+
+  #  it { should have_selector('h1',    text: 'Sign up') }
+  #  it { should have_selector('title', text: full_title('Sign up')) }
+  #end
 
   describe "signup page" do
-    before { visit signup_path }
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it "should have the h1 'Sign up'" do
+      visit signup_path
+      page.should have_selector('h1', text: 'Sign up')
+    end
+
+    it "should have the base title" do
+      visit signup_path
+      page.should have_selector('title',
+                        text: 'Sample App')
+    end
   end
 end
